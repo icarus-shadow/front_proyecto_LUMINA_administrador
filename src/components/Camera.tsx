87338730f1ce560bff3fd9be71f3b0d7e5f5f675
carlos <carlosjuliantarazonaperez@gmail.com>
2025-10-02 21:11:16 -0500

    se crearon los componentes de modal para su uso dinamico en pantallas emergentes
    tambien se implemento camara para su uso en la lectura de qr

---
+++ src/components/Camera.tsx
@@ -0,0 +1,56 @@
+import React, { useEffect, useRef, useState } from "react";
+import "./styles/camera.css"
+
+interface cameraProps {
+    state: boolean;
+}
+
+const Camera: React.FC<cameraProps> = ({state}) => {
+    const videoRef = useRef<HTMLVideoElement | null>(null);
+    const [error, setError] = useState<string | null>(null);
+
+    useEffect(() => {
+        const startCamera = async () => {
+            try {
+                const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+                if (videoRef.current) {
+                    videoRef.current.srcObject = stream;
+                }
+            } catch (err: any) {
+                setError("No se pudo acceder a la cámara: " + err.message);
+            }
+        };
+
+        startCamera();
+
+        return () => {
+            if (videoRef.current?.srcObject) {
+                (videoRef.current.srcObject as MediaStream)
+                    .getTracks()
+                    .forEach(track => track.stop());
+            }
+        };
+    }, []);
+
+    return (
+        <div style={{ textAlign: "center" }}>
+            {error ? (
+                <p>{error}</p>
+            ) : (
+                state ?
+                    (
+                      <div className="sinCamera"></div>
+                    ) : (
+                    <video
+                        ref={videoRef}
+                        autoPlay
+                        playsInline
+                        style={{ width: "100%", maxWidth: "500px", borderRadius: "10px" }}
+                    />
+                    )
+            )}
+        </div>
+    );
+};
+
+export default Camera;
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,11 +1,36 @@
 // @ts-ignore
 import * as React from 'react';
 import {Box} from "@mui/material";
+import Modal from "../components/Modal";
+import Camera from "../components/Camera";
+import "./styles/homeStyles.css";
 
 const Home = () => {
+    const [open, setOpen] = React.useState(false);
+    const [state, setState] = React.useState(false);
+
+    const handleClose = () => {
+        setOpen(false);
+        setState(false);
+    }
+
+    const handleOpen = () => {
+        setOpen(true);
+        setState(true);
+    }
+
     return (
         <Box>
             <h1>Home</h1>
+            <button onClick={handleOpen}>prueba del modal</button>
+            <Modal isOpen={open} onClose={handleClose} >
+                <div className="contModal">
+                    <div className="contMain"></div>
+                    <div className="contCam">
+                        <Camera state={state}/>
+                    </div>
+                </div>
+            </Modal>
         </Box>
     )
 }
