93be3f485eec4ef126a1d837dfe0b3f841c37e03
David Ortiz <judaortizchico@gmail.com>
2025-11-25 21:29:08 -0500

    Nivel de formacion

---
+++ src/services/api/data/LevelFormation.tsx
@@ -0,0 +1,46 @@
+import { instance } from "../baseApi.tsx";
+import type { nivelFormacion } from "../../../types/interfacesData.tsx";
+
+const endpoint = "admin/nivel-formacion"
+
+export const levelFormationApi = {
+    getAll: async function () {
+        try {
+            const response = await instance.get(endpoint);
+            if (response.data) {
+                return response.data;
+            }
+            throw new Error('Invalid response format');
+        } catch (e) {
+            console.error("Error en obtener tipos de programa:", e);
+            throw e;
+        }
+    },
+
+    deleteLevelFormation: async function (id: number) {
+        try {
+            const response = await instance.delete(`${endpoint}/${id}`);
+            if (response.data) {
+                return response.data;
+            }
+            throw new Error('Invalid response format');
+        } catch (e) {
+            console.error("Error en eliminar el tipo de programa:", e);
+            throw e;
+        }
+    },
+
+    addLevelFormation: async function (data: nivelFormacion) {
+        try {
+            const response = await instance.post(endpoint, data);
+            console.log(response);
+            if (response.data) {
+                return response.data;
+            }
+            throw new Error('Invalid response format');
+        } catch (e) {
+            console.error("Error al agregar el tipo de programa:", e);
+            throw e;
+        }
+    }
+}
---
+++ src/services/redux/slices/data/LevelFormationSlice.tsx
@@ -0,0 +1,82 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import { levelFormationApi } from "../../../api/data/LevelFormation.tsx";
+import type { responseDelete, responseLevelFormation } from "../../../../types/interfacesData.tsx";
+
+// Define the initial state using that type
+const initialState: responseLevelFormation & {
+    fetchSuccess: boolean | null;
+    deleteSuccess: boolean | null;
+    addSuccess: boolean | null;
+    updateSuccess: boolean | null;
+} = {
+    fetchSuccess: null,
+    deleteSuccess: null,
+    addSuccess: null,
+    updateSuccess: null,
+    success: null,
+    data: [],
+    count: 0,
+}
+
+export const fetchLevelFormations = createAsyncThunk(
+    'levelFormations/list',
+    async () => { 
+        try {
+            const response = await levelFormationApi.getAll();
+            if (!response.data) {
+                throw new Error('Respuesta inválida del servidor');
+            }
+            return response as responseLevelFormation;
+        } catch (error) {
+            console.error("[levelFormationSlice] error al obtener tipos de programa", error)
+            throw error;
+        }
+    })
+
+export const deleteLevelFormation = createAsyncThunk(
+    'levelFormations/delete',
+    async (id: number, { dispatch }) => {
+        try {
+            const response = await levelFormationApi.deleteLevelFormation(id);
+            console.log(response);
+            if (!response) {
+                throw new Error('Respuesta inválida del servidor');
+            }
+            dispatch(fetchLevelFormations());
+
+            return response as responseDelete;
+        } catch (error) {
+            console.error("[levelFormationSlice] error al eliminar el tipo de programa", error)
+            throw error;
+        }
+    }
+)
+
+export const levelFormationSlice = createSlice({
+    name: 'levelFormations',
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+            //fetch
+            .addCase(fetchLevelFormations.pending, (state) => {
+                state.fetchSuccess = null;
+            })
+            .addCase(fetchLevelFormations.fulfilled, (state, action) => {
+                state.data = action.payload.data;
+                state.fetchSuccess = action.payload.success
+                state.count = state.data?.length || 0;
+            })
+
+            //delete
+            .addCase(deleteLevelFormation.pending, (state) => {
+                state.deleteSuccess = null;
+            })
+            .addCase(deleteLevelFormation.fulfilled, (state, action) => {
+                state.deleteSuccess = action.payload.success
+                state.count = state.data?.length || 0;
+            })
+    }
+})
+
+export default levelFormationSlice.reducer
